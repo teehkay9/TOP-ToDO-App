4ab30f074e9a7c2a6b4be3bdc4fae959c1d6befb
Thomas <thomashoelsken@yahoo.de>
2024-08-27 06:16:51 +0200

    Create basic page layout
    - created basic elements for html
    - installed css loader package
    - added basic styling to css file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import "./style.css";
+
 console.log("Test for webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,29 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
